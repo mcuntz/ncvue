995e3b5340d72d295331bd8647456bb1bfec0f34
Matthias Cuntz <mc@macu.de>
2021-01-19 23:45:02 +0100

    Try mock import of matplotlib.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -44,6 +44,8 @@ def setup(app):
 #
 # needs_sphinx = '1.0'
 
+autodoc_mock_imports = ["matplotlib"]
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
